cbdc1343ac78a3a08258e9ca5d8528fc5c128ca7
mhmadwrekat <mhmmadwreekkat@gmail.com>
2023-09-29 22:21:06 +0300

    add Interactive Rating

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Home from "../component/HomePage/Home";
 const index = () => {
   return (
     <section className="bg-GRAY font-Robot">
-      <p className="bg-PURPLE text-COLDBLUE 2xl:p-8 absolute relative top-10 left-20 z-50 font-Acme font-Robot text-GRAY text-HOTPURPLE text-COLDPURPLE text-WHITE bg-WHITE bg-HOTPURPLE bg-GRAY bg-COLDPURPLE hidden lg:ml-20 2xl:ml-32 p-3 2xl:p-4 lg:p-4 md:p-5 md:p-4 h-screen justify-center align-center items-center bg-orange-500 active:bg-orange-500 focus:text-white focus:bg-orange-500"></p>
+      <p className="bg-PURPLE text-COLDBLUE 2xl:p-8 absolute relative top-10 left-20 z-50 font-Acme font-Robot text-GRAY text-HOTPURPLE text-COLDPURPLE text-WHITE bg-WHITE bg-HOTPURPLE bg-GRAY bg-COLDPURPLE hidden lg:ml-20 2xl:ml-32 p-3 2xl:p-4 lg:p-4 md:p-5 md:p-4 h-screen justify-center align-center items-center bg-orange-500 active:bg-orange-500 focus:text-white lg:p-0 xl:p-0 2xl:p-0 md:m-16 md:p-16 sm:p-10 bg-black focus:bg-orange-500"></p>
       <Nav />
       <section className="text-HOTPURPL w-11/12 md:w-10/12 m-auto">
         <Home />
